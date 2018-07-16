51bfa4cd610aa315cf3e1379a1d35620f66abf23
clare0125 <clare.woozie1@gmail.com>
2018-07-16 16:43:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,30 @@
 
+Song class
+
+attr_accessor :name, :artist, :genre
+
+def initialize
+@@count = 0
+@@genres = []
+@@artists = []
+end
+
+@@count += 1
+  
+  def self.count
+    @@count
+    end
+  end
+  
+  def self.genres
+    @@genres = {}
+  end
+  
+  def self.artist
+    @@artist
+  end
+  
+  def self.genre_count
+    @@genre_count
+  end
+    
